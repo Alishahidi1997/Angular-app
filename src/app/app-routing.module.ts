1c27fc2f8ae043008dd009930e85ef7e3142a113
alish9776 <ali.76sh.97@gmail.com>
2018-08-18 11:00:20 +0430

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WorkdetailComponent} from './workdetail/workdetail.component';
+import {HomeComponent} from './home/home.component';
+import {NotFoundComponent} from  './not-found/not-found.component';
+import {AddComponent} from './add/add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:"/home", pathMatch: 'full'},
+  {path: 'home', component: HomeComponent,
+    children:
+      [{ path:'addcomment' , component:AddComponent }]
+  },
+  {path: 'work/:id', component: WorkdetailComponent},
+  {path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [
+  HomeComponent,
+  WorkdetailComponent,
+  NotFoundComponent,
+  AddComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {DataService} from './data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {routingComponent} from './app-routing.module';
+import {HttpClientModule} from  '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+import { CommentComponent } from './comment/comment.component';
+import { WorkdetailComponent } from './workdetail/workdetail.component';
+import {FormsModule} from '@angular/forms';
+import { MySelectComponent } from './my-select/my-select.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponent,
+    FooterComponent,
+    CommentComponent,
+    WorkdetailComponent,
+    MySelectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled:true})
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
