c269c52f57d4e1c608af9de019a9f22ab812e180
alish9776 <ali.76sh.97@gmail.com>
2018-08-20 11:56:26 +0430

    add style dynamically in angular with webpack(separate js and css)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'exmaple';
+export class AppComponent implements OnInit {
+  public x = false;
+  cssUrl: string;
+  constructor(public sanitizer: DomSanitizer) {
+
+  }
+
+  ngOnInit() {
+    // const randomIntegerInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+    //
+    // const n = randomIntegerInRange(1, 100) % 2;
+    // this.cssUrl = n % 2 === 0 ? '/assets/styles1.css' : '/assets/styles2.css';
+  }
+  add() {
+    if (this.x) {
+      this.cssUrl='/assets/css-files/style1.css';
+    } else {
+      this.cssUrl='/assets/css-files/style2.css';
+    }
+    this.x = !this.x;
+  }
+
 }
